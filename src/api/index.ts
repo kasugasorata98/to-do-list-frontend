d8f9b61e62e6f6cfbdbac20e1e82862b8b286911
kasugasorata98 <jeromykho98@gmail.com>
2023-03-24 01:46:43 +0800

    feat: add authorize api call

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,6 +4,17 @@ import AxiosClient from "@/lib/AxiosClient";
 export const ApiService = {
   getAuthLink: (callback: string) => {
     const query = new URLSearchParams({ callback });
-    return AxiosClient.get(`${Constants.ENDPOINTS.getAuthLink}?${query}`);
+    return AxiosClient.get<{
+      authorizeLink: string;
+    }>(`${Constants.ENDPOINTS.getAuthLink}?${query}`);
+  },
+  authorize: (code: string, callback: string) => {
+    return AxiosClient.post<{
+      access_token: string;
+      refresh_token: string;
+    }>(Constants.ENDPOINTS.authorize, {
+      code,
+      callback,
+    });
   },
 };
