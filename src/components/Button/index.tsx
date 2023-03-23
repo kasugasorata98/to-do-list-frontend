057688c96516402023645f927aa7062246c4a206
kasugasorata98 <jeromykho98@gmail.com>
2023-03-23 23:13:09 +0800

    chore: create reusable components for button and text

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,23 @@
+import { CSSProperties, ReactNode } from "react";
+import { Button as ChakraButton } from "@chakra-ui/react";
+import { colors } from "@/styles/colors";
+
+const Button: React.FC<{
+  style?: CSSProperties | undefined;
+  children?: ReactNode;
+  onClick?: () => void;
+}> = ({ style, children, onClick }) => {
+  return (
+    <ChakraButton
+      style={{
+        color: colors.grey,
+        ...style,
+      }}
+      onClick={onClick}
+    >
+      {children}
+    </ChakraButton>
+  );
+};
+
+export default Button;
---
+++ src/components/Text/index.tsx
@@ -0,0 +1,21 @@
+import { CSSProperties, ReactNode } from "react";
+import { Text as ChakraText } from "@chakra-ui/react";
+import { colors } from "@/styles/colors";
+
+const Text: React.FC<{
+  style?: CSSProperties | undefined;
+  children?: ReactNode;
+}> = ({ style, children }) => {
+  return (
+    <ChakraText
+      style={{
+        color: colors.textColor,
+        ...style,
+      }}
+    >
+      {children}
+    </ChakraText>
+  );
+};
+
+export default Text;
