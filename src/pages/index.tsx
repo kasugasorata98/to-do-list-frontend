4e8ad5e3e20a0803ff4c963d13f4d3b984e1bab2
kasugasorata98 <jeromykho98@gmail.com>
2023-03-23 23:14:00 +0800

    feat: automatically reroute to /login page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,18 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    if (!router.isReady) return;
+    const pathName = router.pathname.slice(1);
+    if (!pathName) {
+      router.push({
+        pathname: "/login",
+      });
+    }
+  }, [router]);
   return (
     <>
       <Head>
