e70a067c4e118dabac71abb0b89f92bf0dec045b
kasugasorata98 <jeromykho98@gmail.com>
2023-03-24 03:53:03 +0800

    chore: setList takes array now, added signOut function

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,8 +5,14 @@ import { ApiService } from "@/api";
 import { ToDoItem } from "@/entities/to-do-list.entity";
 import AddTask from "@/components/AddTaskInput";
 import ToDo from "@/components/ToDo";
+import { GoSignOut } from "react-icons/go";
+import { LocalStorageHandler } from "@/utils/LocalStorageHandler";
+import { useRouter } from "next/router";
+import Icon from "@/components/Icon";
+
 const Home = () => {
   const [list, setList] = useState<Array<ToDoItem>>([]);
+  const router = useRouter();
   useEffect(() => {
     ApiService.getList()
       .then(({ data }) => {
@@ -16,20 +22,25 @@ const Home = () => {
         console.log(err);
       });
   }, []);
+
   const addToList = (title: string) => {
     ApiService.addToList(title)
       .then(({ data }) => {
-        setList({
-          ...list,
-          ...data,
-        });
+        setList([...list, data]);
       })
       .catch((err) => {
         console.log(err);
       });
   };
+
+  const signOut = () => {
+    LocalStorageHandler.removeUserToken();
+    router.push("/login");
+  };
+
   return (
     <VStack paddingBlock={20}>
+      <Icon as={GoSignOut} onClick={signOut} />
       <AddTask onEnterPressed={(text) => addToList(text)} />
       {list.map((item: ToDoItem) => {
         return <ToDo key={item._id} item={item} />;
