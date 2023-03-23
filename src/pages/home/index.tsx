fbc9934b1be917f2265c48507f7d59e29d3eb88d
kasugasorata98 <jeromykho98@gmail.com>
2023-03-24 02:28:36 +0800

    chore: added basic jsx for to do list

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,76 @@
 import withAuth from "@/utils/withAuth";
-
+import {
+  InputGroup,
+  InputLeftElement,
+  Input,
+  HStack,
+  Checkbox,
+  VStack,
+} from "@chakra-ui/react";
+import { DragHandleIcon, SettingsIcon } from "@chakra-ui/icons";
+import { colors } from "@/styles/colors";
+import Text from "@/components/Text";
+import { useEffect, useState } from "react";
+import { ApiService } from "@/api";
+import { ToDoList } from "@/entities/to-do-list.entity";
 const Home = () => {
-  return <></>;
+  const [list, setList] = useState<Array<ToDoList>>([]);
+  useEffect(() => {
+    ApiService.getList()
+      .then(({ data }) => {
+        setList(data.toDoList);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  return (
+    <VStack>
+      <InputGroup
+        borderColor={colors.dividerColor}
+        borderRadius={8}
+        background={colors.dividerColor}
+      >
+        <InputLeftElement
+          pointerEvents="none"
+          children={<DragHandleIcon color="gray.300" />}
+        />
+        <Input
+          type="text"
+          color={colors.textColor}
+          placeholder="Add a task..."
+        />
+      </InputGroup>
+      {list.map((item: any) => {
+        return (
+          <HStack key={item._id} width={"100%"}>
+            <Checkbox
+              checked={item.isDone}
+              style={{
+                alignSelf: "start",
+                marginTop: 4,
+                marginLeft: 2,
+              }}
+            />
+            <Text
+              style={{
+                width: "100%",
+              }}
+            >
+              {item.title}
+            </Text>
+            <SettingsIcon
+              _hover={{
+                cursor: "pointer",
+              }}
+              onClick={() => console.log}
+              color={colors.textColor}
+            />
+          </HStack>
+        );
+      })}
+    </VStack>
+  );
 };
 
 export default withAuth(Home);
