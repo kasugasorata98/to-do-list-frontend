39ec7a60255e44a0bbd2e5f24ca2e6031b8325a3
kasugasorata98 <jeromykho98@gmail.com>
2023-03-24 03:18:36 +0800

    chore: removed unwanted dependencies and setList for response

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,18 +1,10 @@
 import withAuth from "@/utils/withAuth";
-import {
-  InputGroup,
-  InputLeftElement,
-  Input,
-  HStack,
-  Checkbox,
-  VStack,
-} from "@chakra-ui/react";
-import { DragHandleIcon, SettingsIcon } from "@chakra-ui/icons";
-import { colors } from "@/styles/colors";
-import Text from "@/components/Text";
+import { VStack } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { ApiService } from "@/api";
 import { ToDoItem } from "@/entities/to-do-list.entity";
+import AddTask from "@/components/AddTaskInput";
+import ToDo from "@/components/ToDo";
 const Home = () => {
   const [list, setList] = useState<Array<ToDoItem>>([]);
   useEffect(() => {
@@ -24,50 +16,23 @@ const Home = () => {
         console.log(err);
       });
   }, []);
+  const addToList = (title: string) => {
+    ApiService.addToList(title)
+      .then(({ data }) => {
+        setList({
+          ...list,
+          ...data,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
-    <VStack>
-      <InputGroup
-        borderColor={colors.dividerColor}
-        borderRadius={8}
-        background={colors.dividerColor}
-      >
-        <InputLeftElement
-          pointerEvents="none"
-          children={<DragHandleIcon color="gray.300" />}
-        />
-        <Input
-          type="text"
-          color={colors.textColor}
-          placeholder="Add a task..."
-        />
-      </InputGroup>
+    <VStack paddingBlock={20}>
+      <AddTask onEnterPressed={(text) => addToList(text)} />
       {list.map((item: ToDoItem) => {
-        return (
-          <HStack key={item._id} width={"100%"}>
-            <Checkbox
-              checked={item.isDone}
-              style={{
-                alignSelf: "start",
-                marginTop: 4,
-                marginLeft: 2,
-              }}
-            />
-            <Text
-              style={{
-                width: "100%",
-              }}
-            >
-              {item.title}
-            </Text>
-            <SettingsIcon
-              _hover={{
-                cursor: "pointer",
-              }}
-              onClick={() => console.log}
-              color={colors.textColor}
-            />
-          </HStack>
-        );
+        return <ToDo key={item._id} item={item} />;
       })}
     </VStack>
   );
