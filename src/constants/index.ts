e96ab51f5cafc61e34f264d289e20bd062c8ffca
kasugasorata98 <jeromykho98@gmail.com>
2023-03-24 03:18:44 +0800

    chore: add endpoint

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,5 +3,6 @@ export const Constants = {
     getAuthLink: "/api/v1/account/getAuthLink",
     authorize: "/api/v1/account/authorize",
     getList: "/api/v1/list",
+    addToList: "/api/v1/list",
   },
 };
