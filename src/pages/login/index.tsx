2b4ccb5b4024638a605e0ec00e0384e5e089ada7
kasugasorata98 <jeromykho98@gmail.com>
2023-03-24 01:49:23 +0800

    feat: added handleSignIn function for login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,36 +1,35 @@
 import { colors } from "@/styles/colors";
-import { Box, Center, Heading } from "@chakra-ui/react";
+import { Box, Heading } from "@chakra-ui/react";
 import Text from "@/components/Text";
 import Button from "@/components/Button";
+import MainLayout from "@/layout/MainLayout";
+import { ApiService } from "@/api";
 
 const Login = () => {
-  const handleSignIn = () => {};
+  const handleSignIn = () => {
+    ApiService.getAuthLink("http://localhost:3000/callback")
+      .then(({ data }) => {
+        window.location.href = data.authorizeLink;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
-    <Center h="100vh" bg={colors.grey}>
-      <Box
-        maxW="md"
-        w="full"
-        borderWidth="1px"
-        borderRadius="4"
-        overflow="hidden"
-        p="8"
-        borderColor={colors.grey}
-        bg={colors.grey}
+    <Box maxW="md" w="full" overflow="hidden" p="8">
+      <Heading as="h2" size="lg" mb="4" color={colors.textColor}>
+        Login
+      </Heading>
+      <Text
+        style={{
+          marginBottom: 4,
+        }}
       >
-        <Heading as="h2" size="lg" mb="4" color={colors.textColor}>
-          Login
-        </Heading>
-        <Text
-          style={{
-            marginBottom: 4,
-          }}
-        >
-          Please sign in to continue.
-        </Text>
-        <Button onClick={handleSignIn}>Login with Google</Button>
-      </Box>
-    </Center>
+        Please sign in to continue.
+      </Text>
+      <Button onClick={handleSignIn}>Login with Google</Button>
+    </Box>
   );
 };
 
