78b7bb7d3f8289bbf89b624fb9d537877f5c05aa
kasugasorata98 <jeromykho98@gmail.com>
2023-03-24 01:48:01 +0800

    chore: move mainlayout to be used in app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import "../styles/globals.css";
+import MainLayout from "@/layout/MainLayout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
     </ChakraProvider>
   );
 }
