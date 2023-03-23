0ed615a95c073f96e4238d5e2c53ce4f8d1c62e0
kasugasorata98 <jeromykho98@gmail.com>
2023-03-24 01:49:08 +0800

    feat: added HOC to protect routes

---
+++ src/utils/withAuth.tsx
@@ -0,0 +1,20 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { LocalStorageHandler } from "./LocalStorageHandler";
+
+const withAuth = (WrappedComponent: React.ComponentType<any>) => {
+  const Wrapper = (props: any) => {
+    const router = useRouter();
+
+    useEffect(() => {
+      const tokens = LocalStorageHandler.getUserToken();
+      if (!tokens) router.push("/login");
+    }, [router]);
+
+    return <WrappedComponent {...props} />;
+  };
+
+  return Wrapper;
+};
+
+export default withAuth;
